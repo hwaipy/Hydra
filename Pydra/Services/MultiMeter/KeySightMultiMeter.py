fd17cd3547922ea18555b4713a1c4ff7ecff64c9
Hwaipy Li <hwaipy@gmail.com>
2017-12-31 23:54:10 +0800

    Add KeySight34470A in Pydra

--- Pydra/Services/MultiMeter/KeySightMultiMeter.py
+++ Pydra/Services/MultiMeter/KeySightMultiMeter.py
@@ -3,9 +3,48 @@ __author__ = 'Hwaipy'
 from Instruments import DeviceException, VISAInstrument
 import enum
 
+
 class KeySight_MultiMeter_34465A(VISAInstrument):
     manufacturer = 'Keysight Technologies'
     model = '34465A'
+
+    def __init__(self, resourceID):
+        super().__init__(resourceID)
+
+    def setMeasureQuantity(self, mq, range=0, autoRange=True, aperture=0.001):
+        if mq is MeasureQuantity.VoltageDC:
+            self.scpi.CONF.VOLT.DC.write('AUTO' if autoRange else range)
+            self.scpi.VOLT.APER.write(aperture)
+        elif mq is MeasureQuantity.CurrentDC:
+            self.scpi.CONF.CURR.DC.write('AUTO' if autoRange else range)
+            self.scpi.CURR.APER.write(aperture)
+        else:
+            raise DeviceException('MeasureQuantity {} can not be recognized.'.format(mq))
+
+    def directMeasure(self, count=1):
+        self.scpi.TRIG.SOURCE.write('BUS')
+        self.scpi.SAMP.COUN.write(count)
+        self.scpi.INIT.write()
+        self.scpi._TRG.write()
+        values = self.scpi.FETC.query()
+        return [float(v) for v in values.split(',')]
+
+    def directMeasureAndFetchLater(self, count=1):
+        self.scpi.TRIG.SOURCE.write('BUS')
+        self.scpi.SAMP.COUN.write(count)
+        self.scpi.INIT.write()
+        self.scpi._TRG.write()
+
+        def fetch():
+            values = self.scpi.FETC.query()
+            return [float(v) for v in values.split(',')]
+
+        return fetch
+
+
+class KeySight_MultiMeter_34470A(VISAInstrument):
+    manufacturer = 'Keysight Technologies'
+    model = '34470A'
 
     def __init__(self, resourceID):
         super().__init__(resourceID)
--- Pydra/soap/LaserStabilityTest.py
+++ Pydra/soap/LaserStabilityTest.py
@@ -1,4 +1,4 @@
-from Services.MultiMeter.KeySight34465A import KeySight_MultiMeter_34465A, MeasureQuantity
+from Services.MultiMeter.KeySightMultiMeter import KeySight_MultiMeter_34465A, KeySight_MultiMeter_34470A, MeasureQuantity
 from Pydra import Session
 from Services.Storage import StorageService, HBTFileElement
 import time
@@ -8,20 +8,25 @@ if __name__ == '__main__':
     dev1 = KeySight_MultiMeter_34465A('TCPIP0::192.168.25.103::inst0::INSTR')
     dev1.setMeasureQuantity(MeasureQuantity.VoltageDC, autoRange=False, range=0.1, aperture=1)
     dev2 = KeySight_MultiMeter_34465A('TCPIP0::192.168.25.107::inst0::INSTR')
-    dev2.setMeasureQuantity(MeasureQuantity.VoltageDC, autoRange=False, range=0.3, aperture=1)
+    dev2.setMeasureQuantity(MeasureQuantity.VoltageDC, autoRange=False, range=0.05, aperture=1)
+    dev3 = KeySight_MultiMeter_34470A('TCPIP0::192.168.25.52::inst0::INSTR')
+    dev3.setMeasureQuantity(MeasureQuantity.CurrentDC, autoRange=False, range=0.02, aperture=1)
 
     service = StorageService(session)
     timeStamp = time.strftime("%Y-%m-%d %H-%M-%S", time.localtime())
     hbtFile = service.getElement(
         '/Hwaipy/4K_EOE/20171230-LaserStability/{}.hip/VoltageRecord.hbt'.format(timeStamp)).toHBTFileElement()
     hbtFile.initialize(
-        [["Time (ms)", HBTFileElement.LONG], ["PD1 (V)", HBTFileElement.DOUBLE], ["PD2 (V)", HBTFileElement.DOUBLE]])
+        [["Time (ms)", HBTFileElement.LONG], ["PD1 (V)", HBTFileElement.DOUBLE], ["PD2 (V)", HBTFileElement.DOUBLE],
+         ["LD Current(A)", HBTFileElement.DOUBLE]])
 
     while True:
         f1 = dev1.directMeasureAndFetchLater(count=1)
         f2 = dev2.directMeasureAndFetchLater(count=1)
+        f3 = dev3.directMeasureAndFetchLater(count=1)
         r1 = f1()[0]
         r2 = f2()[0]
+        r3 = f3()[0]
         currentTime = time.time()
-        hbtFile.appendRow([currentTime * 1000, r1, r2])
-        print([currentTime * 1000, r1, r2])
+        hbtFile.appendRow([currentTime * 1000, r1, r2, r3])
+        print([currentTime * 1000, r1, r2, r3])
