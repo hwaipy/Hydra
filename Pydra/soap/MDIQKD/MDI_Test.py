4c24df2c04b10c0d3e954b66a99b711a9dd2f95f
Hwaipy Li <hwaipy@gmail.com>
2018-11-12 17:25:44 +0800

    Updating Pydra with MDI Test

---
+++ Pydra/soap/MDIQKD/MDI_Test.py
@@ -0,0 +1,45 @@
+import Pydra
+import time
+
+class ExperimentControl:
+    def __init__(self):
+        self.session = Pydra.Session.newSession(('192.168.25.27', 20102), None, 'MDI-QKD-Controller-Test')
+        self.currentTDCReportSize = -1
+
+    def getCurrentTDCReport(self, timeout = 5):
+        storage = self.session.blockingInvoker("StorageService")
+        path = '/test/tdc/mdireport.fs'
+        startTime = time.time()
+        while True:
+            currentSize = int(storage.metaData("", path, False)['Size'])
+            if self.currentTDCReportSize == -1:
+                self.currentTDCReportSize = currentSize
+                continue
+            if self.currentTDCReportSize >= currentSize:
+                if timeout <= 0:
+                    time.sleep(0.3)
+                    continue
+                else:
+                    timePass = time.time() - startTime
+                    if timePass > timeout:
+                        return None
+            else: break
+
+        frameBytes = storage.FSFileReadTailFrames("", path, 0, 1)
+
+
+        # val mg = new MessageGenerator()
+        # mg.feed(frameBytes)
+
+    def stop(self):
+        self.session.stop()
+
+
+if __name__ == '__main__':
+    ec = ExperimentControl()
+
+    while True:
+        print(ec.getCurrentTDCReport())
+        break
+
+    ec.stop()
